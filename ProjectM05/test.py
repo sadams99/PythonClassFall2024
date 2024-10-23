3c45236fa516caeb45fef68a86421eda2c92faaa
Scheri <130200745+sadams99@users.noreply.github.com>
2024-10-22 20:19:41 -0400

    Add files via upload

---
+++ ProjectM05/test.py
@@ -0,0 +1,31 @@
+from fractions import Fraction
+
+import unittest
+
+from my_sum import sum
+
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+    def test_list_fraction(self):
+        """
+        Test that it can sum a list of fractions
+        """
+        data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
+        result = sum(data)
+        self.assertEqual(result, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+##The results cause it to fail and crash because in the code we are demanding it to have an answer of 1.
+# The code cannot mathmatically make that true. 
